f8a58188192c4abc4512d13e5fdfa3d296b9509c
JSaurav0409 <sauravjha0409@gmail.com>
2024-08-10 16:19:39 +0530

    More details about conversion

--- 01_Basics/3_ConversionOperator.js
+++ 01_Basics/3_ConversionOperator.js
@@ -1,12 +1,12 @@
 let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
-console.log(typeof (valueInNumber));
+// console.log(typeof (valueInNumber));
 
 // Result
 
@@ -18,7 +18,7 @@ console.log(typeof (valueInNumber));
 let isLoggedIn = "hello";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // Result
 
@@ -29,5 +29,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let someString = String(someNumber)
-console.log(someString);
-console.log(typeof(someString));
+// console.log(someString);
+// console.log(typeof(someString));
+
+
+// **** OPERATIONS ****
+
+let value = 33 
+let negativeValue = -value
+// console.log(negativeValue);
+
+// Basic Operators
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello"
+let str2 = " World"
+// console.log(str1 + str2);
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 1);
+// console.log(1 + "1");
+// console.log(1 + 1 + "1");
+// console.log("1" + 2 + 2);
+
+// console.log( 2 + 1 * 3 % 5 );  X
+// console.log( (2 + 1) * 3 % 5 ); O (Write your code as simple as possible)
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2   // Not a good way to wriye a code
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
