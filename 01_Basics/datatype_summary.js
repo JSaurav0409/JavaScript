274c9c9d054e49bb809207a121336ff03f27d7f0
JSaurav0409 <sauravjha0409@gmail.com>
2024-08-12 13:17:05 +0530

    Datatype_Summary

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,75 @@
+// Datatypes are maninly of two type : Primitive and Reference(Non-Primitive)
+
+// Primitive types consists 7 datatype
+/*
+ * String
+ * Number    
+ * Boolean
+ * Null
+ * undefined
+ * Symbol
+ * BigInt
+*/
+
+// String
+let userName = "Saurav Jha"
+console.log(typeof (userName)); // String
+
+// Numbers
+const score = 100
+const scoreValue = 110.23
+console.log(typeof (score)); // Numbers
+console.log(typeof (scoreValue)); // Numbers
+
+// Boolean
+let isLoggedIn = false
+console.log(typeof (isLoggedIn)); // Boolean
+
+// Null
+let outsideTemperatures = null
+console.log(typeof (outsideTemperatures)); // Object
+
+
+// Undefined
+let userEmail;
+console.log(typeof (userEmail)); // undefined
+
+
+// Symbol 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+console.log(typeof (id)); // Symbol
+console.log(typeof (anotherId)); // Symbol
+
+
+// BigInt
+const bigIntValue = 1000000000000000000n
+console.log(typeof (bigIntValue)); // bigint
+
+
+
+// Reference Type (Non-Primitive type):
+/*
+ * Array
+ * Object
+ * Functions
+*/
+
+const Heroes = ["IronMan", "Captain America", "SpiderMan"];
+console.log(typeof (Heroes)); // Object
+
+
+let myObj = {
+    name: "Saurav Jha",
+    age: 23,
+    isLoggedIn: false,
+}
+console.log(typeof (myObj)); // Object
+
+const myFunc = function () {
+    console.log("Hello World");
+}
+
+console.log(typeof (myFunc)); // Function
